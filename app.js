5d453815536b98218c9e91520f15d51e6c953722
iryna-popkova <131870449+iryna-popkova@users.noreply.github.com>
2024-01-29 14:11:30 +0200

    mongoose

--- app.js
+++ app.js
@@ -3,6 +3,12 @@ import morgan from "morgan";
 import cors from "cors";
 
 import contactsRouter from "./routes/contactsRouter.js";
+import mongoose from "mongoose";
+
+//6Hs9ifpI0WiK1hEu
+
+const DB_HOST =
+  "mongodb+srv://Iryna:6Hs9ifpI0WiK1hEu@cluster0.ihchbuc.mongodb.net/db-contacts?retryWrites=true&w=majority";
 
 const app = express();
 
@@ -21,6 +27,12 @@ app.use((err, req, res, next) => {
   res.status(status).json({ message });
 });
 
-app.listen(3000, () => {
-  console.log("Server is running. Use our API on port: 3000");
-});
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    app.listen(3000);
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
