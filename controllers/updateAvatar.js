6f62f3dff934531d0a3b14e0707b0c5e0ddeea41
iryna-popkova <131870449+iryna-popkova@users.noreply.github.com>
2024-02-12 23:33:58 +0200

    Update updateAvatar.js

--- controllers/updateAvatar.js
+++ controllers/updateAvatar.js
@@ -7,19 +7,20 @@ const avatarsDir = path.join(__dirname, "../", "../", "public", "avatars");
 
 const updateAvatar = async (req, res) => {
   const { _id } = req.user;
-  const { path: tempUpload, originalname } = req.file;
+  const { path: tmpUpload, originalname } = req.file;
+
   const filename = `${_id}_${originalname}`;
 
-  const image = await Jimp.read(tempUpload);
-  image.resize(250, 250);
-
   const resultUpload = path.join(avatarsDir, filename);
+  await fs.rename(tmpUpload, resultUpload);
+
+  const image = await Jimp.read(resultUpload);
+  await image.resize(250, 250);
   await image.writeAsync(resultUpload);
-  await fs.unlink(tempUpload);
 
   const avatarURL = path.join("avatars", filename);
-
   await User.findByIdAndUpdate(_id, { avatarURL });
+
   res.status(200).json({
     avatarURL,
   });
